5b51881a8a4913f74418d06ef5c10cd3d7dd5b02
AliAlievALive <madarat@mail.ru>
2022-02-09 15:36:40 +0300

    add file upload controller

--- src/main/java/com/halal/awsimageupload/controller/UserProfileController.java
+++ src/main/java/com/halal/awsimageupload/controller/UserProfileController.java
@@ -3,11 +3,12 @@ package com.halal.awsimageupload.controller;
 import com.halal.awsimageupload.profile.UserProfile;
 import com.halal.awsimageupload.service.UserProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/user-profile")
@@ -22,5 +23,15 @@ public class UserProfileController {
     @GetMapping
     public List<UserProfile> getUserProfile() {
         return userProfileService.getUserProfiles();
+    }
+
+    @PostMapping(
+            path = "{userProfileId}/image/upload",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public void uploadUserProfileImage(@PathVariable("userProfileId") UUID userProfileId,
+                                       @RequestParam("file") MultipartFile file) {
+        userProfileService.uploadUserProfileImage(userProfileId, file);
     }
 }
