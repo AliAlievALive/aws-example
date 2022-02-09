3767bed6c19cedf9cec15cef662a04ed95e43366
AliAlievALive <madarat@mail.ru>
2022-02-09 19:30:24 +0300

    implement TODOs in uploadUserProfileImage

--- src/main/java/com/halal/awsimageupload/service/UserProfileService.java
+++ src/main/java/com/halal/awsimageupload/service/UserProfileService.java
@@ -1,20 +1,32 @@
 package com.halal.awsimageupload.service;
 
+import com.halal.awsimageupload.bucket.BucketName;
+import com.halal.awsimageupload.filestore.FileStore;
 import com.halal.awsimageupload.profile.UserProfile;
+import org.apache.http.entity.ContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-import java.util.UUID;
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.apache.http.entity.ContentType.*;
 
 @Service
 public class UserProfileService {
     private final UserProfileDataAccessService userProfileDataAccessService;
+    private final FileStore fileStore;
 
     @Autowired
-    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService) {
+    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService, FileStore fileStore) {
         this.userProfileDataAccessService = userProfileDataAccessService;
+        this.fileStore = fileStore;
     }
 
     public List<UserProfile> getUserProfiles() {
@@ -22,11 +34,35 @@ public class UserProfileService {
     }
 
     public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
-// TODO
-//      1. Check if image is not empty
-//      2. If file is an image
-//      3. The user exists in our DB
-//      4. Grab some metadata from file if any
-//      5. Store the image in s3 and update DB (userProfileImageLink) with s3 image link
+
+        //      1. Check if image is not empty
+        if (file.isEmpty()) {
+            throw new IllegalStateException("Cannot upload empty file");
+        }
+
+        //      2. If file is an image
+        if (!List.of(IMAGE_JPEG, IMAGE_PNG, IMAGE_GIF).contains(file.getContentType())) {
+            throw new IllegalStateException("File must be an image");
+        }
+
+        //      3. The user exists in our DB
+        UserProfile user = userProfileDataAccessService.getUserProfiles().stream()
+                .filter(userProfile -> userProfile.getUserProfileId().equals(userProfileId))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException(String.format("User profile %s not found", userProfileId)));
+
+        //      4. Grab some metadata from file if any
+        Map<String, String> metadata = new HashMap<>();
+        metadata.put("Content-Type", file.getContentType());
+        metadata.put("Content-Length", String.valueOf(file.getSize()));
+
+        //      5. Store the image in s3 and update DB (userProfileImageLink) with s3 image link
+        String path = String.format("%s/%s", BucketName.PROFILE_IMAGE.getBucketName(), user.getUserProfileId());
+        String fileName = String.format("%s-%s", file.getName(), UUID.randomUUID());
+        try {
+            fileStore.save(path, fileName, Optional.of(metadata), file.getInputStream());
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
