7248be6a80f6336b5edd50c170aa0b973099b929
AliAlievALive <madarat@mail.ru>
2022-02-09 15:44:52 +0300

    add checklist for TODO

--- src/main/java/com/halal/awsimageupload/service/UserProfileService.java
+++ src/main/java/com/halal/awsimageupload/service/UserProfileService.java
@@ -22,6 +22,11 @@ public class UserProfileService {
     }
 
     public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
-
+// TODO
+//      1. Check if image is not empty
+//      2. If file is an image
+//      3. The user exists in our DB
+//      4. Grab some metadata from file if any
+//      5. Store the image in s3 and update DB (userProfileImageLink) with s3 image link
     }
 }
