05786aedad8f96aab3750246e5d39494ea02960c
AliAlievALive <madarat@mail.ru>
2022-02-09 14:10:57 +0300

    add fake user profile data store

--- src/main/java/com/halal/awsimageupload/profile/UserProfile.java
+++ src/main/java/com/halal/awsimageupload/profile/UserProfile.java
@@ -1,6 +1,7 @@
 package com.halal.awsimageupload.profile;
 
 import java.util.Objects;
+import java.util.Optional;
 import java.util.UUID;
 
 public class UserProfile {
@@ -30,8 +31,8 @@ public class UserProfile {
         this.userName = userName;
     }
 
-    public String getUserProfileImageLink() {
-        return userProfileImageLink;
+    public Optional<String> getUserProfileImageLink() {
+        return Optional.ofNullable(userProfileImageLink);
     }
 
     public void setUserProfileImageLink(String userProfileImageLink) {
